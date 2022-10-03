f5ff0191353de79dae252abc1a2e2a4e2743c732
jstzwj88 <110.3870790@qq.com>
2022-10-04 00:50:23 +0800

    MultiScaleDiscriminator

--- vits/model/vits.py
+++ vits/model/vits.py
@@ -1,4 +1,5 @@
 
+import itertools
 from typing import Dict
 import torch
 from torch import nn
@@ -7,6 +8,8 @@ from torch import optim
 
 
 import pytorch_lightning as pl
+
+from vits.model.multi_scale_discriminator import MultiScaleDiscriminator
 
 from .synthesizer_trn import SynthesizerTrn
 from .multi_period_discriminator import MultiPeriodDiscriminator
@@ -26,7 +29,8 @@ class VITS(pl.LightningModule):
             self.hparams.data.filter_length // 2 + 1,
             self.hparams.train.segment_size // self.hparams.data.hop_length,
             **self.hparams.model)
-        self.net_d = MultiPeriodDiscriminator(self.hparams.model.use_spectral_norm)
+        self.net_period_d = MultiPeriodDiscriminator(self.hparams.model.use_spectral_norm)
+        self.net_scale_d = MultiScaleDiscriminator(self.hparams.model.use_spectral_norm)
 
         self.generator_out = None
 
@@ -60,23 +64,40 @@ class VITS(pl.LightningModule):
                 self.hparams.data.mel_fmax
             )
 
-            y_d_hat_r, y_d_hat_g, fmap_r, fmap_g = self.net_d(y, y_hat)
-            loss_dur = torch.sum(l_length.float())
-            loss_mel = F.l1_loss(y_mel, y_hat_mel) * self.hparams.train.c_mel
-            loss_kl = kl_loss(z_p, logs_q, m_p, logs_p, z_mask) * self.hparams.train.c_kl
+            y_dp_hat_r, y_dp_hat_g, fmap_p_r, fmap_p_g = self.net_period_d(y, y_hat)
+            
+            loss_p_fm = feature_loss(fmap_p_r, fmap_p_g)
+            loss_p_gen, losses_p_gen = generator_loss(y_dp_hat_g)
 
-            loss_fm = feature_loss(fmap_r, fmap_g)
-            loss_gen, losses_gen = generator_loss(y_d_hat_g)
-            loss_gen_all = loss_gen + loss_fm + loss_mel + loss_dur + loss_kl
+            y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = self.net_scale_d(y, y_hat)
+
+            loss_s_fm = feature_loss(fmap_s_r, fmap_s_g)
+            loss_s_gen, losses_s_gen = generator_loss(y_ds_hat_g)
+
+            # kl
+            loss_kl = kl_loss(z_p, logs_q, m_p, logs_p, z_mask) * self.hparams.train.c_kl
+            # dur
+            loss_dur = torch.sum(l_length.float())
+            # mel
+            loss_mel = F.l1_loss(y_mel, y_hat_mel) * self.hparams.train.c_mel
+
+            loss_gen_all = (loss_s_gen + loss_s_fm) + (loss_p_gen + loss_p_fm) + loss_kl + loss_mel + loss_dur
 
             grad_norm_g = commons.clip_grad_value_(self.net_g.parameters(), None)
 
             # Logging to TensorBoard by default
             lr = self.optim_g.param_groups[0]['lr']
             scalar_dict = {"loss/g/total": loss_gen_all, "learning_rate": lr, "grad_norm_g": grad_norm_g}
-            scalar_dict.update({"loss/g/fm": loss_fm, "loss/g/mel": loss_mel, "loss/g/dur": loss_dur, "loss/g/kl": loss_kl})
+            scalar_dict.update({
+                "loss/g/p_fm": loss_p_fm,
+                "loss/g/s_fm": loss_s_fm,
+                "loss/g/mel": loss_mel,
+                "loss/g/dur": loss_dur,
+                "loss/g/kl": loss_kl
+            })
 
-            scalar_dict.update({"loss/g/{}".format(i): v for i, v in enumerate(losses_gen)})
+            scalar_dict.update({"loss/g/p_gen_{}".format(i): v for i, v in enumerate(losses_p_gen)})
+            scalar_dict.update({"loss/g/s_gen_{}".format(i): v for i, v in enumerate(losses_s_gen)})
 
             image_dict = { 
                 "slice/mel_org": utils.plot_spectrogram_to_numpy(y_mel[0].data.cpu().numpy()),
@@ -98,17 +119,25 @@ class VITS(pl.LightningModule):
             y_hat, l_length, attn, ids_slice, x_mask, z_mask, (z, z_p, m_p, logs_p, m_q, logs_q) = self.generator_out
             y = commons.slice_segments(y, ids_slice * self.hparams.data.hop_length, self.hparams.train.segment_size) # slice 
             
-            y_d_hat_r, y_d_hat_g, _, _ = self.net_d(y, y_hat.detach())
-            loss_disc, losses_disc_r, losses_disc_g = discriminator_loss(y_d_hat_r, y_d_hat_g)
-            loss_disc_all = loss_disc
+            # MPD
+            y_d_hat_r, y_d_hat_g, _, _ = self.net_period_d(y, y_hat.detach())
+            loss_disc_p, losses_disc_p_r, losses_disc_p_g = discriminator_loss(y_d_hat_r, y_d_hat_g)
 
-            grad_norm_d = commons.clip_grad_value_(self.net_d.parameters(), None)
+            # MSD
+            y_ds_hat_r, y_ds_hat_g, _, _ = self.net_scale_d(y, y_hat.detach())
+            loss_disc_s, losses_disc_s_r, losses_disc_s_g = discriminator_loss(y_ds_hat_r, y_ds_hat_g)
+            loss_disc_all = loss_disc_p + loss_disc_s
+
+            grad_norm_p_d = commons.clip_grad_value_(self.net_period_d.parameters(), None)
+            grad_norm_s_d = commons.clip_grad_value_(self.net_scale_d.parameters(), None)
 
             # log
             lr = self.optim_g.param_groups[0]['lr']
-            scalar_dict = {"loss/d/total": loss_disc_all, "learning_rate": lr, "grad_norm_d": grad_norm_d}
-            scalar_dict.update({"loss/d_r/{}".format(i): v for i, v in enumerate(losses_disc_r)})
-            scalar_dict.update({"loss/d_g/{}".format(i): v for i, v in enumerate(losses_disc_g)})
+            scalar_dict = {"loss/d/total": loss_disc_all, "learning_rate": lr, "grad_norm_p_d": grad_norm_p_d, "grad_norm_s_d": grad_norm_s_d}
+            scalar_dict.update({"loss/d_p_r/{}".format(i): v for i, v in enumerate(losses_disc_p_r)})
+            scalar_dict.update({"loss/d_p_g/{}".format(i): v for i, v in enumerate(losses_disc_p_g)})
+            scalar_dict.update({"loss/d_s_r/{}".format(i): v for i, v in enumerate(losses_disc_s_r)})
+            scalar_dict.update({"loss/d_s_g/{}".format(i): v for i, v in enumerate(losses_disc_s_g)})
 
             image_dict = {}
             
@@ -184,7 +213,7 @@ class VITS(pl.LightningModule):
             betas=self.hparams.train.betas, 
             eps=self.hparams.train.eps)
         self.optim_d = torch.optim.AdamW(
-            self.net_d.parameters(),
+            itertools.chain(self.net_period_d.parameters(), self.net_scale_d.parameters()),
             self.hparams.train.learning_rate, 
             betas=self.hparams.train.betas, 
             eps=self.hparams.train.eps)
