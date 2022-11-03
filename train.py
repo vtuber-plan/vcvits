687e1a5a7d241267011b70a76c9dae7ce5b6dcc0
jstzwj <1103870790@qq.com>
2022-11-03 20:49:50 +0800

    update argparse

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 from vits.hparams import HParams
-from vits.data.dataset.preload_vc_ms import PreloadAnyVoiceConversionMultiSpeakerLoader
+from vits.data.dataset.preload_vc_ms import PreloadAnyVoiceConversionMultiSpeakerDataset
 
 def get_hparams(config_path: str) -> HParams:
     with open(config_path, "r") as f:
@@ -34,18 +34,20 @@ def main():
     parser.add_argument('-c', '--config', type=str, default="./configs/base.json", help='JSON file for configuration')
     parser.add_argument('-a', '--accelerator', type=str, default="gpu", help='training device')
     parser.add_argument('-d', '--device', type=str, default="0", help='training device ids')
+    parser.add_argument('-s', '--skip-preprocess', action='store_true', help='Skip preprocess')
     args = parser.parse_args()
 
     hparams = get_hparams(args.config)
     pl.utilities.seed.seed_everything(hparams.train.seed)
 
-    train_dataset = PreloadAnyVoiceConversionMultiSpeakerLoader(hparams.data.training_files, hparams.data)
-    valid_dataset = PreloadAnyVoiceConversionMultiSpeakerLoader(hparams.data.validation_files, hparams.data)
+    train_dataset = PreloadAnyVoiceConversionMultiSpeakerDataset(hparams.data.training_files, hparams.data)
+    valid_dataset = PreloadAnyVoiceConversionMultiSpeakerDataset(hparams.data.validation_files, hparams.data)
 
-    preprocess(hparams, hparams.data.training_files, hparams.data.source_sampling_rate, load_features=True)
-    preprocess(hparams, hparams.data.training_files, hparams.data.target_sampling_rate)
-    preprocess(hparams, hparams.data.validation_files, hparams.data.source_sampling_rate, load_features=True)
-    preprocess(hparams, hparams.data.validation_files, hparams.data.target_sampling_rate)
+    if "skip-preprocess" not in args:
+        preprocess(hparams, hparams.data.training_files, hparams.data.source_sampling_rate, load_features=True)
+        preprocess(hparams, hparams.data.training_files, hparams.data.target_sampling_rate)
+        preprocess(hparams, hparams.data.validation_files, hparams.data.source_sampling_rate, load_features=True)
+        preprocess(hparams, hparams.data.validation_files, hparams.data.target_sampling_rate)
     
     collate_fn = PreloadAnyVoiceConversionMultiSpeakerCollate()
     train_loader = DataLoader(train_dataset, batch_size=hparams.train.batch_size, num_workers=16, shuffle=True, pin_memory=True, collate_fn=collate_fn)
--- vits/data/dataset/preload_vc_ms.py
+++ vits/data/dataset/preload_vc_ms.py
@@ -15,8 +15,9 @@ import torchaudio
 from vits.mel_processing import MAX_WAV_VALUE, mel_spectrogram_torch, spec_to_mel_torch, spectrogram_torch
 from vits.utils import load_filepaths, load_wav_to_torch, load_filepaths_and_text
 from ..audio import get_audio
+import tqdm
 
-class PreloadAnyVoiceConversionMultiSpeakerLoader(torch.utils.data.Dataset):
+class PreloadAnyVoiceConversionMultiSpeakerDataset(torch.utils.data.Dataset):
     def __init__(self, audiopaths: str, hparams):
         self.audiopaths = load_filepaths_and_text(audiopaths)
         self.hparams = hparams
@@ -78,6 +79,80 @@ class PreloadAnyVoiceConversionMultiSpeakerLoader(torch.utils.data.Dataset):
             "y_pitch": y_pitch_mel,
             "y_hubert_features": y_hubert_features,
         }
+
+    def __len__(self):
+        return len(self.audiopaths)
+
+
+class MemoryPreloadAnyVoiceConversionMultiSpeakerDataset(torch.utils.data.Dataset):
+    def __init__(self, audiopaths: str, hparams):
+        self.audiopaths = load_filepaths_and_text(audiopaths)
+        self.hparams = hparams
+        self.max_wav_value  = hparams.max_wav_value
+        self.source_sampling_rate  = hparams.source_sampling_rate
+        self.target_sampling_rate  = hparams.target_sampling_rate
+        self.filter_length  = hparams.filter_length
+        self.hop_length     = hparams.hop_length
+        self.win_length     = hparams.win_length
+
+        self.resamplers = {}
+
+        random.seed(1234)
+        random.shuffle(self.audiopaths)
+        self.dataset = []
+        self.preload()
+    
+    def preload(self):
+        for index, item in enumerate(tqdm.tqdm(self.audiopaths)):
+            audiopath = item[0]
+            if len(item) == 1:
+                sid = 0
+            else:
+                sid = int(item[1])
+            x_spec, x_wav, x_melspec, x_pitch_mel, x_hubert_features = get_audio(
+                audiopath,
+                max_wav_value = self.max_wav_value,
+                filter_length = self.filter_length,
+                hop_length = self.hop_length,
+                win_length = self.win_length,
+                n_mel_channels = self.hparams.n_mel_channels,
+                mel_fmin = self.hparams.mel_fmin,
+                mel_fmax = self.hparams.mel_fmax,
+                hubert_channels = self.hparams.hubert_channels,
+                num_pitch = self.hparams.num_pitch,
+                sr=self.source_sampling_rate, load_features=True)
+            y_spec, y_wav, y_melspec, y_pitch_mel, y_hubert_features = get_audio(
+                audiopath,
+                max_wav_value = self.max_wav_value,
+                filter_length = self.filter_length,
+                hop_length = self.hop_length,
+                win_length = self.win_length,
+                n_mel_channels = self.hparams.n_mel_channels,
+                mel_fmin = self.hparams.mel_fmin,
+                mel_fmax = self.hparams.mel_fmax,
+                hubert_channels = self.hparams.hubert_channels,
+                num_pitch = self.hparams.num_pitch,
+                sr=self.target_sampling_rate, load_features=False)
+            self.dataset.append(
+                {
+                    "sid": sid,
+
+                    "x_spec": x_spec,
+                    "x_wav": x_wav,
+                    "x_mel": x_melspec,
+                    "x_pitch": x_pitch_mel,
+                    "x_hubert_features": x_hubert_features,
+
+                    "y_spec": y_spec,
+                    "y_wav": y_wav,
+                    "y_mel": y_melspec,
+                    "y_pitch": y_pitch_mel,
+                    "y_hubert_features": y_hubert_features,
+                }
+            )
+
+    def __getitem__(self, index):
+        return self.dataset[index]
 
     def __len__(self):
         return len(self.audiopaths)
